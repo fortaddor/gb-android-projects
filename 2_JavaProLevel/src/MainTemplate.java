698c5a67c1ab5a1fb12abe62bb7af4f6f5fe95ee
Valerij <my_computer@web.de>
2020-07-27 23:18:26 +0200

    Add MainTemplate.java

---
+++ 2_JavaProLevel/src/MainTemplate.java
@@ -0,0 +1,31 @@
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MainTemplate
+{
+    public static void main(String[] args)
+    {
+        arrayLists();
+    }
+
+    private static void arrayLists()
+    {
+        List<Integer> numbers = new ArrayList<Integer>();
+        System.out.println(numbers);
+
+        // trimToSize()
+        // ensureCapacity()
+    }
+
+    private static int getCapacity(ArrayList list)
+    {
+//    try
+//    {
+//        Field elementData = ArrayList.class.ge
+//    }
+
+        return 0;
+    }
+}
