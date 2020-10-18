67c17e5e1e9c72e0db630eb5d0fb403474da4ba8
Valerij <my_computer@web.de>
2020-10-18 18:03:48 +0200

    Hausaufgabe 1 / Lesson 1

--- 4_AndroidCore/1_Lesson/HelloActivity/app/src/main/java/com/example/helloactivity/MainActivity.java
+++ 4_AndroidCore/1_Lesson/HelloActivity/app/src/main/java/com/example/helloactivity/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.helloactivity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView helloTextView = findViewById(R.id.hello_text);
+        if (helloTextView == null)
+        {
+            return;
+        }
+
+        helloTextView.setText(getString(R.string.hello_text) + " :))");
     }
 }
